775dcb243ffd821fb860bb1fddb33080b829c196
devarthuraugusto <arthuraugusto2102@icloud.com>
2026-09-09 18:46:08 -0300

    feat: alteracoes feitas em aula

--- LoginGalo/src/main/java/com/galo/LoginGalo/application/LoginGaloApplication.java
+++ LoginGalo/src/main/java/com/galo/LoginGalo/application/LoginGaloApplication.java
@@ -1,9 +1,9 @@
-package com.galo.LoginGalo;
+package com.galo.LoginGalo.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.galo"})
 public class LoginGaloApplication {
 
 	public static void main(String[] args) {
--- LoginGalo/src/test/java/com/galo/LoginGalo/LoginGaloApplicationTests.java
+++ LoginGalo/src/test/java/com/galo/LoginGalo/LoginGaloApplicationTests.java
@@ -3,7 +3,7 @@ package com.galo.LoginGalo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes={com.galo.LoginGalo.application.LoginGaloApplication.class})
 class LoginGaloApplicationTests {
 
 	@Test
