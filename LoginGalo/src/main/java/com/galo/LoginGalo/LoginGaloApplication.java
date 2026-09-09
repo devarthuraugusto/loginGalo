dd3d61ebd962a4c9e24ce574bb99846d6674abea
devarthuraugusto <arthuraugusto2102@icloud.com>
2026-09-09 17:51:06 -0300

    Estrutura adicionada

---
+++ LoginGalo/src/main/java/com/galo/LoginGalo/LoginGaloApplication.java
@@ -0,0 +1,13 @@
+package com.galo.LoginGalo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginGaloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginGaloApplication.class, args);
+	}
+
+}
---
+++ LoginGalo/src/test/java/com/galo/LoginGalo/LoginGaloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galo.LoginGalo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginGaloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
